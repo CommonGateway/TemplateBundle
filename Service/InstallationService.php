0ca68108bcee2528b29ca9c5978c74ebe0e6e459
Barry Brands <57346398+bbrands02@users.noreply.github.com>
2022-12-14 00:16:20 +0100

    Installation fixes

--- Service/InstallationService.php
+++ Service/InstallationService.php
@@ -51,41 +51,61 @@ class InstallationService implements InstallerInterface
     {
 
         // Lets create some genneric dashboard cards
-        $objectsThatShouldHaveCards = ['https://common-gateway.nl/template-group.schema.json', 'https://common-gateway.nl/template.schema.json'];
+        // $objectsThatShouldHaveCards = ['https://common-gateway.nl/template-group.schema.json', 'https://common-gateway.nl/template.schema.json'];
 
-        foreach ($objectsThatShouldHaveCards as $object) {
-            (isset($this->io) ? $this->io->writeln('Looking for a dashboard card for: ' . $object) : '');
-            $entity = $this->entityManager->getRepository('App:Entity')->findOneBy(['reference' => $object]);
-            if (
-                $entity &&
-                !$dashboardCard = $this->entityManager->getRepository('App:DashboardCard')->findOneBy(['entityId' => $entity->getId()])
-            ) {
-                $dashboardCard = new DashboardCard($entity);
-                $this->entityManager->persist($dashboardCard);
-                (isset($this->io) ? $this->io->writeln('Dashboard card created') : '');
-                continue;
-            }
-            (isset($this->io) ? $this->io->writeln('Dashboard card found') : '');
-        }
+        // foreach ($objectsThatShouldHaveCards as $object) {
+        //     (isset($this->io) ? $this->io->writeln('Looking for a dashboard card for: ' . $object) : '');
+        //     $entity = $this->entityManager->getRepository('App:Entity')->findOneBy(['reference' => $object]);
+        //     if (
+        //         $entity &&
+        //         !$dashboardCard = $this->entityManager->getRepository('App:DashboardCard')->findOneBy(['entityId' => $entity->getId()])
+        //     ) {
+        //         $dashboardCard = new DashboardCard($entity);
+        //         $this->entityManager->persist($dashboardCard);
+        //         (isset($this->io) ? $this->io->writeln('Dashboard card created') : '');
+        //         continue;
+        //     }
+        //     (isset($this->io) ? $this->io->writeln('Dashboard card found') : '');
+        // }
 
         // Let create some endpoints
-        $objectsThatShouldHaveEndpoints = ['https://common-gateway.nl/template-group.schema.json', 'https://common-gateway.nl/template.schema.json'];
+        // $objectsThatShouldHaveEndpoints = ['https://common-gateway.nl/template-group.schema.json', 'https://common-gateway.nl/template.schema.json'];
 
-        foreach ($objectsThatShouldHaveEndpoints as $object) {
-            (isset($this->io) ? $this->io->writeln('Looking for a endpoint for: ' . $object) : '');
-            $entity = $this->entityManager->getRepository('App:Entity')->findOneBy(['reference' => $object]);
+        // foreach ($objectsThatShouldHaveEndpoints as $object) {
+        //     (isset($this->io) ? $this->io->writeln('Looking for a endpoint for: ' . $object) : '');
+        //     $entity = $this->entityManager->getRepository('App:Entity')->findOneBy(['reference' => $object]);
 
-            if (
-                $entity &&
-                count($entity->getEndpoints()) == 0
-            ) {
-                $endpoint = new Endpoint($entity);
-                $this->entityManager->persist($endpoint);
-                (isset($this->io) ? $this->io->writeln('Endpoint created') : '');
-                continue;
-            }
-            (isset($this->io) ? $this->io->writeln('Endpoint found') : '');
-        }
+        //     if (
+        //         $entity &&
+        //         count($entity->getEndpoints()) == 0
+        //     ) {
+        //         $endpoint = new Endpoint($entity);
+        //         $this->entityManager->persist($endpoint);
+        //         (isset($this->io) ? $this->io->writeln('Endpoint created') : '');
+        //         continue;
+        //     }
+        //     (isset($this->io) ? $this->io->writeln('Endpoint found') : '');
+        // }
+
+        $schemaRepository = $this->entityManager->getRepository('App:Entity');
+        $templateGroup = $schemaRepository->findOneBy(['name' => 'TemplateGroup']);
+
+        $endpoint = $endpointRepository->findOneBy(['name' => 'TemplateGroups collection']) ?? new Endpoint();
+        $endpoint->setName('TemplateGroups collection');
+        $endpoint->setPathRegex('^(template_groups)$');
+        $endpoint->setMethods(["POST", "GET"]);
+        $endpoint->setMethod("POST");
+        $endpoint->setEntity($templateGroup);
+        $endpoint->setOperationType('collection');
+
+        $endpoint = $endpointRepository->findOneBy(['name' => 'TemplateGroups item']) ?? new Endpoint();
+        $endpoint->setName('TemplateGroups item');
+        $endpoint->setPathRegex('^(template_groups/[a-z0-9-]{36})$');
+        $endpoint->setMethods(["PUT", "GET"]);
+        $endpoint->setMethod("POST");
+        $endpoint->setEntity($templateGroup);
+        $endpoint->setOperationType('collection');
+
 
         $this->entityManager->flush();
 
